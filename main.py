58bd8dddc0a57f7e10043ce082b830d2b64b885e
Nyzlo4ka <nyzlo4ka@mail.ru>
2024-12-07 15:29:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+number_str = '123.456'
+number_float = float(number_str)
+shifted_number = number_float * 10
+first_digit = int(shifted_number) % 10
+print(first_digit)
